5f1ddb11d0e6aba383ea3ce701e283bc50ebcb27
bu1bzy <zhiqing.liew@outlook.com>
2025-12-12 19:39:58 +0800

    Add analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import { Toaster } from "@/components/ui/toaster";
+import { Analytics } from "@vercel/analytics/react";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
@@ -28,6 +29,7 @@ const App = () => (
         </Routes>
       </BrowserRouter>
     </TooltipProvider>
+    <Analytics />
   </QueryClientProvider>
 );
 
